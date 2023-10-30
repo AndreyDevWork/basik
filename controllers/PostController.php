eee9117ab97f325cf64ad8f5e54c58ca21f1ed76
Andrey <andrey.develop.a@gmail.com>
2023-10-30 23:00:42 +0700

    Работа с базой данных, селекты

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -31,11 +31,19 @@ class PostController extends Controller
         $model = new PostForm();
         $products = new Product();
         $rows = PostForm::find()->all();
+        $count = PostForm::find()->count();
+        $one = PostForm::find()->asArray()->one(); // Запрос будет избыточным (Лучше лимит всегда)
         $where = PostForm::find()->where(['name' => 'RUSSIA'])->all();
-        $like = PostForm::find()->asArray()->where(['like', 'select', 'g'])->all();
+        $like = PostForm::find()->asArray()->where(['like', 'select', 'g'])->limit(2)->all();
         $moreThan = PostForm::find()->asArray()->where(['>=', 'id', '50'])->all();
         $sortDesc = Product::find()->orderBy(['id' => SORT_DESC])->all();
         $asArray = Product::find()->asArray()->all();
-        return $this->render('data_base', compact('model', 'rows', 'products', 'sortDesc', 'asArray', 'where', 'like', 'moreThan'));
+        $findOne = PostForm::findOne(['name' => 'RUSSIA']);
+        $findOne = PostForm::findAll(['name' => 'RUSSIA']);
+//        $bySql = "SELECT * FROM post WHERE `select` LIKE '%ra%'"; НЕЛЬЗЯ, УЯЗВИМОЙ К ИНЪЕКЦИЯМ
+//        $bySqlSelect = PostForm::findBySql($bySql)->asArray()->all();
+        $bySql = "SELECT * FROM post WHERE `select` LIKE :search";
+        $bySqlSelect = PostForm::findBySql($bySql, [':search' => '%ra%'])->asArray()->all(); // ТАК ПРАВИЛЬНО
+        return $this->render('data_base', compact('model', 'rows', 'products', 'sortDesc', 'asArray', 'where', 'like', 'moreThan', 'one', 'count', 'bySqlSelect'));
     }
 }
--- views/post/data_base.php
+++ views/post/data_base.php
@@ -3,9 +3,12 @@
 /** @var app\models\PostForm $model */
 /** @var \yii\db\ActiveQuery $rows */
 /** @var \yii\db\ActiveQuery $where */
+/** @var \yii\db\ActiveQuery $one */
+/** @var \yii\db\ActiveQuery $count */
 /** @var \yii\db\ActiveQuery $moreThan */
 /** @var \yii\db\ActiveQuery $like */
-/** @var \yii\db\ActiveQuery $products */
+/** @var \yii\db\ActiveQuery $bySqlSelect */
+/** @var app\models\Product $products */
 /** @var \yii\db\ActiveQuery $sortDesc */
 /** @var \yii\db\ActiveQuery $asArray */
 
@@ -17,9 +20,13 @@
 <p></p>
 <?php
 dump($rows);
-dump($sortDesc);
-dump($asArray);
 dump($where);
 dump($like);
 dump($moreThan);
+dump($sortDesc);
+dump($asArray);
+dump($one);
+dump($count);
+dump($bySqlSelect);
+
 ?>
